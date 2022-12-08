5b24ea4c5139bbd021609bcfb4337e770b802a73
Samira Rodrigues Pinheiro <86476435+Samira300-coder@users.noreply.github.com>
2022-12-08 11:01:27 -0300

    Super-Trunfo

---
+++ snk-battlesuper-trunfo/README.md
@@ -0,0 +1,4 @@
+# SnK Battle- Super Trunfo.
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Samynya/pen/eYgvYed](https://codepen.io/Samynya/pen/eYgvYed).
+
---
+++ snk-battlesuper-trunfo/src/script.js
@@ -0,0 +1,329 @@
+
+var cartaEren = {
+	nome: "Eren Yeager",
+  imagem: "https://i.pinimg.com/originals/47/7f/22/477f223f490204b24b0752520b4fad2c.png",
+	atributos: {
+		ataque: 90,
+		defesa: 88,
+		magia: 95
+	}
+}
+
+var cartaMikasa = {
+	nome: "Mikasa Ackerman",
+  imagem: "https://64.media.tumblr.com/01a3b0150ca6a02edc46eede298fd12c/a5ffc76656af69e6-57/s640x960/f3d8df3ad8b63a868211a4eabd389ba8c4bd912b.png",
+	atributos: {
+		ataque: 95,
+		defesa: 75,
+		magia: 90
+	}
+}
+
+var cartaLevi = {
+	nome: "Levi Ackerman",
+  imagem: "https://data.whicdn.com/images/320522270/original.jpg",
+	atributos: {
+		ataque: 100,
+		defesa: 82,
+		magia: 90
+	}
+}
+
+var cartaArmin = {
+	nome: "Armin Arlert",
+	imagem: "https://i.pinimg.com/originals/a7/c2/52/a7c252e4857933ef3b09e05b16dc73ee.jpg",
+	atributos: {
+		ataque: 65,
+		defesa: 69,
+		magia: 90
+	}
+}
+
+var cartaSasha = {
+	nome: "Sasha Braus",
+	imagem: "https://64.media.tumblr.com/598ce033ea1b7130f8c1cb237638186b/a7e6357916354807-35/s1280x1920/fdc34970ccf11b9b20e3a3e48a35cd0deba560c2.jpg",
+	atributos: {
+		ataque: 80,
+		defesa: 70,
+		magia: 10
+	}
+}
+
+var cartaJean = {
+	nome: "Jean Kirstein",
+	imagem: "https://64.media.tumblr.com/6308aa31fdd5673464a833952835ac68/2db4cdab317810e4-fb/s500x750/7087205fc08b2c606d20f0acbd3930306e84c7ae.jpg",
+	atributos: {
+		ataque: 70,
+		defesa: 50,
+		magia: 15
+	}
+}
+
+var cartaConnie = {
+	nome: "Connie Springer",
+	imagem: "https://i.pinimg.com/originals/ce/7b/0a/ce7b0a2faa72ae6fc46abd59065308fa.jpg",
+	atributos: {
+		ataque: 50,
+		defesa: 60,
+		magia: 15
+	}
+}
+
+var cartaAnnie = {
+	nome: "Annie Leonhart",
+	imagem: "https://i.pinimg.com/originals/5c/b4/68/5cb4681ad829534bac8b48ce3b8d84f1.jpg",
+	atributos: {
+		ataque: 90,
+		defesa: 86,
+		magia: 75
+	}
+}
+
+var cartaPieck = {
+ nome: "Pieck Finger",
+ imagem: "https://64.media.tumblr.com/9c1b3b8968e443f8458289110ce4ed3c/ebca5c96c12981c2-a0/s400x600/e38c311b1d29ae214de113048a1b2faeb23fc83d.png",
+ atributos: {
+ ataque: 60,
+ defesa: 65, 
+ magia: 75
+  }
+}
+
+var cartaReiner = {
+ nome: "Reiner Braun", 
+ imagem: "https://pbs.twimg.com/profile_images/1269262004314873857/9IepnO1p.jpg", 
+ atributos: {
+  ataque: 78,
+  defesa: 80,
+  magia: 75
+ }
+}
+
+var cartaFalco = {
+ nome: "Falco Grice", 
+ imagem: "https://i.pinimg.com/originals/f6/a6/d1/f6a6d150d9f0138446fc46c41281403f.jpg", 
+  atributos: {
+  ataque: 64,
+  defesa: 60,
+  magia: 70
+  }
+}
+
+var cartaGabi = {
+ nome: "Gabi Braun",
+ imagem: "https://i.pinimg.com/originals/cb/d2/d1/cbd2d1e42f6f6d3e8b75e40ffb4e902d.jpg",
+ atributos: {
+  ataque: 76,
+  defesa: 60,
+  magia: 15
+ }
+}
+
+var cartaBertholdt = {
+ nome: "Bertholdt Hoover",
+ imagem: "https://i.pinimg.com/736x/d9/27/75/d9277589954ec71c563195523780350c.jpg", 
+ atributos: {
+  ataque: 60,
+  defesa: 63,
+  magia: 84
+ }  
+}
+
+var cartaZeke = {
+ nome: "Zeke Yeager",
+ imagem: "https://i.pinimg.com/originals/02/3e/c0/023ec0e0e71ad1ace08d6fa2d4ddd1dc.jpg",
+ atributos: {
+  ataque: 76,
+  defesa: 70,
+  magia: 87
+ }
+}
+
+var cartaPorco = {
+ nome: "Porco Galliard",
+ imagem: "https://64.media.tumblr.com/d66191ffb8ca8f0264fba51c3fb10d49/96aa29ce4202c9d9-ad/s640x960/8655dc954e5646975414e80d3987e5bc5a23e582.jpg",
+ atributos: {
+ ataque: 60,
+ defesa: 65,
+ magia: 80 
+ }
+}
+
+var cartaErwin = {
+ nome: "Erwin Smith",
+ imagem: "https://i.pinimg.com/originals/54/e8/eb/54e8ebc3cff10016cadc03eec791fbe0.png",
+ atributos: {
+ ataque: 70,
+ defesa: 80,
+ magia: 10
+ } 
+}
+
+var cartaHange = {
+ nome: "Hange Zoe",
+ imagem: "https://i.pinimg.com/originals/95/59/33/955933d6888e1149e209cf6d82dd0a76.jpg",
+ atributos: {
+ ataque: 75,
+ defesa: 78,
+ magia: 12
+ }
+}
+
+var cartaYmir = {
+ nome: "Ymir",
+ imagem: "https://64.media.tumblr.com/bfc56c0feb809235d8a39fa3b05908dd/tumblr_pl0ztlKW0W1vetel5_1280.png",
+ atributos: {
+ ataque: 80,
+ defesa: 70,
+ magia: 76
+ }
+}
+
+var cartaFritz = {
+ nome: "Fundadora Ymir Fritz",
+ imagem: "https://64.media.tumblr.com/5b44555123bf23756379329fa4a67a03/e91ca45123b8d0c0-4e/s1280x1920/cad6a7c350e36114f844e99ebb474e8393b50fa6.jpg",
+ atributos: {
+  ataque: 100, 
+  defesa: 80,
+  magia: 110
+ }
+}
+
+var cartaHistoria = {
+ nome: "Historia Reiss", 
+ imagem: "https://i.pinimg.com/474x/19/10/db/1910db24acf7124995ce7e1029e2d072.jpg", 
+  atributos: {
+  ataque: 40,
+  defesa: 30,
+   magia: 70
+  }
+}
+
+var cartaMaquina
+var cartaJogador
+var cartas = [cartaEren, cartaMikasa, cartaLevi, cartaArmin, cartaSasha, cartaJean, cartaConnie, cartaAnnie, cartaPieck, cartaReiner, cartaFalco, cartaGabi, cartaBertholdt, cartaZeke, cartaPorco, cartaErwin, cartaHange, cartaYmir, cartaFritz, cartaHistoria]
+
+var pontosJogador = 0
+var pontosMaquina = 0
+
+atualizaPlacar()
+atualizaQuantidadeDeCartas()
+
+function atualizaQuantidadeDeCartas() {
+	var divQuantidadeCartas = document.getElementById('quantidade-cartas')
+	var html = "Quantidade de cartas no jogo: " + cartas.length
+	
+	divQuantidadeCartas.innerHTML = html
+}
+
+function atualizaPlacar() {
+	var divPlacar = document.getElementById('placar')
+	var html = "Jogador " + pontosJogador + "/" + pontosMaquina + " Máquina"
+	
+	divPlacar.innerHTML = html
+}
+
+function sortearCarta() {
+	var numeroCartaMaquina = parseInt(Math.random() * cartas.length)
+	cartaMaquina = cartas [numeroCartaMaquina]
+  cartas.splice(numeroCartaMaquina, 1)
+	
+	var numeroCartaJogador = parseInt(Math.random() * cartas.length)
+	cartaJogador = cartas[numeroCartaJogador]
+  cartas.splice(numeroCartaJogador, 1)
+  
+	document.getElementById('btnSortear').disabled = true
+	document.getElementById('btnJogar').disabled = false
+  
+  exibeCartaJogador()
+}
+
+function exibeCartaJogador() {
+    var divCartaJogador = document.getElementById('carta-jogador')
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">'
+    divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type='radio' name='atributo' checked value='" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status'>"
+
+    divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function obtemAtributoSelecionado() {
+	var radioAtributo = document.getElementsByName('atributo')
+  
+	for (var i = 0; i < radioAtributo.length; i++) {
+		if (radioAtributo[i].checked) {
+			return radioAtributo[i].value
+		}
+	}
+}
+
+function jogar() {
+  var divResultado = document.getElementById('resultado')
+	var atributoSelecionado = obtemAtributoSelecionado()
+	
+	if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+		htmlResultado = '<p class="resultado-final">Venceu</p>'
+    pontosJogador++
+	} else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+		htmlResultado = '<p class="resultado-final">Perdeu</p>'
+    pontosMaquina++
+	} else {
+		htmlResultado = '<p class="resultado-final">Empatou</p>'
+	}
+  
+  if (cartas.length == 0) {
+    alert("Fim de jogo!")
+    if (pontosJogador > pontosMaquina) {
+      htmlResultado = '<p class="resultado-final">Venceu</p>'
+    } else if (pontosMaquina > pontosJogador) {
+      htmlResultado = '<p class="resultado-final">Perdeu</p>'
+    } else {
+      htmlResultado = '<p class="resultado-final">Empatou</p>'
+    }
+  } else {
+    document.getElementById('btnProximaRodada').disabled = false
+  }
+  
+  divResultado.innerHTML = htmlResultado
+  document.getElementById('btnJogar').disabled = true
+  
+  atualizaPlacar()
+  exibeCartaMaquina()
+  atualizaQuantidadeDeCartas()
+}
+
+function exibeCartaMaquina() {
+    var divCartaMaquina = document.getElementById('carta-maquina')
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">'
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaMaquina.atributos) {
+        opcoesTexto += "<p type='text' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status --spacing'>"
+
+    divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function proximaRodada() {
+	var divCartas = document.getElementById('cartas')
+	
+	divCartas.innerHTML = `<div id="carta-jogador" class="carta"></div> <div id="carta-maquina" class="carta"></div>`
+	
+	document.getElementById('btnSortear').disabled = false
+	document.getElementById('btnJogar').disabled = true
+	document.getElementById('btnProximaRodada').disabled = true
+	
+	var divResultado = document.getElementById('resultado')
+	divResultado.innerHTML = ""
+}
